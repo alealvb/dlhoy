369a8a4649d52557616d1d1d2901d19a22641e6d
Alejandro Alvarez <alejandro.alvarez.coder@gmail.com>
2022-08-13 16:57:17 -0300

    [#4] Change lang to es in html tag

--- app/root.tsx
+++ app/root.tsx
@@ -31,7 +31,7 @@ export async function loader({ request }: LoaderArgs) {
 
 export default function App() {
   return (
-    <html lang="en" className="h-full">
+    <html lang="es" className="h-full">
     <head>
       <Meta />
       <Links />
